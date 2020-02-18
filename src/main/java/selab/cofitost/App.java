b1029578f494628bcbc9dc008cc91fe9504d6782
cofitost <zxcv091922@gmail.com>
2020-02-19 00:58:48 +0800

    = =

--- src/main/java/selab/cofitost/App.java
+++ src/main/java/selab/cofitost/App.java
@@ -1,13 +1,35 @@
 package selab.cofitost;
 
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    Capabilities chromeCapabilities = DesiredCapabilities.chrome();
+    Capabilities firefoxCapabilities = DesiredCapabilities.firefox();
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        RemoteWebDriver chrome = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), chromeCapabilities);
+        RemoteWebDriver firefox = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), firefoxCapabilities);
+    
+        // run against chrome
+        chrome.get("https://www.google.com");
+        System.out.println(chrome.getTitle());
+    
+        // run against firefox
+        firefox.get("https://www.google.com");
+        System.out.println(firefox.getTitle());
+      
+        chrome.quit();
+        firefox.quit();
     }
 }
