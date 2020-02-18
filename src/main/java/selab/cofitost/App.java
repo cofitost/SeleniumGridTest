374b3b2a11bef64de934e02e4a9bf21ae363b4cf
cofitost <zxcv091922@gmail.com>
2020-02-18 18:40:36 +0800

    init

---
+++ src/main/java/selab/cofitost/App.java
@@ -0,0 +1,13 @@
+package selab.cofitost;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
